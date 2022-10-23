094c8f42feda57b66c3fb1e2dfb6315db655ed83
TomasLey <113106085+TomasLey@users.noreply.github.com>
2022-10-23 23:09:36 +0200

    Update milestone3.py

--- sources/milestone3/milestone3.py
+++ sources/milestone3/milestone3.py
@@ -21,25 +21,21 @@ t_fin = 20
 N1 = 2000                            # Condiciones iniciales para Euler Inverso, ya que 
 t1 = linspace(0, t_fin, N1+1)        # da problemas de convergencia para dt = 0.1
 
-N2 = 200
+N2 = 200                             # Condiciones iniciales para el resto de esquemas
 t2 = linspace(0, t_fin, N2+1)
 
 ## Problem definition
 
+Error_y_convergencia(Cauchy, Kepler, t1, U0, Euler_Inv, q_order = 1,mpoints = 8)
 
-#Error_y_convergencia(Cauchy, Kepler, t1, U0, Euler_Inv, q_order = 1,mpoints = 8)
-Richardson(Cauchy, Kepler, t1, U0, Euler_Inv, q_order = 1, iter = 2)
 
-mpoints = [14,10,8]         # Número de puntos evaluados en el gráfico log(Error) - log(N)
-temp_sch = [Euler, RK4, CN]
-q_order = [1.,4.,2.]
-
-iter = 3
+mpoints = [14, 10, 8]           # Número de puntos evaluados en el gráfico log(Error) - log(N)
+temp_sch = [Euler, RK4, CN]     # Esquemas temporales
+q_order = [1.,4.,2.]            # Orden de convergencia de los esquemas
 
 for scheme in temp_sch:
 
-    Richardson(Cauchy, Kepler, t2, U0, scheme, q_order[temp_sch.index(scheme)], iter)
-    #Error_y_convergencia(Cauchy, Kepler, t2, U0, scheme, q_order[temp_sch.index(scheme)],mpoints[temp_sch.index(scheme)])
+    Error_y_convergencia(Cauchy, Kepler, t2, U0, scheme, q_order[temp_sch.index(scheme)],mpoints[temp_sch.index(scheme)])
 
 
 
